3f743cd5f82a81f822e79ac5eb81e292f04247af
uyudane <sy.yun@icloud.com>
2022-10-21 23:06:09 +0900

    Add: metadataを追加

---
+++ frontend_proadmaps/src/component/meta.tsx
@@ -0,0 +1,44 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import siteImg from '../../public/images/proad_icon.jpg';
+import { siteMeta } from 'lib/constants';
+
+const { siteTitle, siteDesc, siteUrl, siteLocale, siteType, siteIcon } = siteMeta;
+
+export const Meta = ({ pageTitle, pageDesc }: { pageTitle?: string; pageDesc?: string }) => {
+  // ページのタイトル
+  const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle;
+
+  // ページの説明
+  const desc = pageDesc ?? siteDesc;
+
+  // ページのURL
+  const router = useRouter();
+  const url = `${siteUrl}${router.asPath}`;
+
+  // OGP画像
+  const img = siteImg.src;
+  const imgW = String(siteImg.width);
+  const imgH = String(siteImg.height);
+  const imgUrl = `${siteUrl}${img}`;
+
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta property='og:title' content={title} />
+      <meta name='description' content={desc} />
+      <meta property='og:description' content={desc} />
+      <link rel='canonical' href={url} />
+      <meta property='og:url' content={url} />
+      <meta property='og:site_name' content={siteTitle} />
+      <meta property='og:type' content={siteType} />
+      <meta property='og:locale' content={siteLocale} />
+      <link rel='icon' href={siteIcon} />
+      <link rel='apple-touch-icon' href={siteIcon} />
+      <meta property='og:image' content={imgUrl} />
+      <meta property='og:image:width' content={imgW} />
+      <meta property='og:image:height' content={imgH} />
+      <meta name='twitter:card' content='summary_large_image' />
+    </Head>
+  );
+};
---
+++ frontend_proadmaps/src/lib/constants.ts
@@ -0,0 +1,9 @@
+export const siteMeta = {
+  siteTitle: 'ProadMaps',
+  siteDesc: 'プログラミング学習のロードマップを検索&共有する。',
+  siteUrl: 'https://proadmaps.vercel.app/',
+  siteLang: 'ja',
+  siteLocale: 'ja_JP',
+  siteType: 'website',
+  siteIcon: '/proad_icon.png',
+};
