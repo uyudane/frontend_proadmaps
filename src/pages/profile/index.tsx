713c7241ab9c251373332768e58b3a63ae4e55a1
uyudane <sy.yun@icloud.com>
2022-10-27 09:24:12 +0900

    Add: プロフィール情報を更新できるように修正(ログインをし直さないとうまく認証されなかったため、要確認(Auth0のログイン済みと、recoilでのtoken情報に差分が出ている??))

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,22 +1,20 @@
-import { Button, Container, Stack, TextField, Grid, Box, Typography } from '@mui/material';
+import { Button, Container, Stack, TextField, Grid } from '@mui/material';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { useRecoilValue } from 'recoil';
 import Meta from 'component/meta';
 import UserIcon from 'component/user_icon';
-
-// フォームの型
-interface SampleFormInput {
-  name: string;
-  github: string;
-  twitter: string;
-}
+import tokenState from 'recoil/atoms/tokenState';
+import { postProfiles } from 'services/profiles';
+import type { Profile } from 'types';
 
 function ProfilePage() {
-  const { register, handleSubmit } = useForm<SampleFormInput>();
+  const { register, handleSubmit } = useForm<Profile>();
+  const token = useRecoilValue(tokenState); // RecoilのTokneを取得する
 
   // フォーム送信時の処理
-  const onSubmit: SubmitHandler<SampleFormInput> = (data) => {
+  const onSubmit: SubmitHandler<Profile> = (data) => {
     // バリデーションチェックOK！なときに行う処理を追加
-    console.log(data);
+    postProfiles(data, token);
   };
   return (
     <>
@@ -38,7 +36,7 @@ function ProfilePage() {
             <Stack spacing={4}>
               <Grid container>
                 <Grid item xs={12}>
-                  ・ユーザ名
+                  ・ユーザ名(必須)
                 </Grid>
                 <Grid item xs={12}>
                   <TextField
@@ -51,7 +49,7 @@ function ProfilePage() {
               </Grid>
               <Grid container>
                 <Grid item xs={12}>
-                  ・公開用Githubアカウント
+                  ・公開用Githubアカウント(任意)
                 </Grid>
                 <Grid item xs={3}>
                   {/* <Typography component='span'>https://github.com/</Typography> */}
@@ -65,13 +63,13 @@ function ProfilePage() {
                   <TextField
                     // label='GitHubID'
                     sx={{ width: '100%', bgcolor: '#ffffff' }}
-                    {...register('github')}
+                    {...register('github_account')}
                   />
                 </Grid>
               </Grid>
               <Grid container>
                 <Grid item xs={12}>
-                  ・公開用Twitterアカウント
+                  ・公開用Twitterアカウント(任意)
                 </Grid>
                 <Grid item xs={3}>
                   <TextField
@@ -81,7 +79,10 @@ function ProfilePage() {
                   />
                 </Grid>
                 <Grid item xs={9}>
-                  <TextField sx={{ width: '100%', bgcolor: '#ffffff' }} {...register('twitter')} />
+                  <TextField
+                    sx={{ width: '100%', bgcolor: '#ffffff' }}
+                    {...register('twitter_account')}
+                  />
                 </Grid>
               </Grid>
               <Button
--- src/types/data.d.ts
+++ src/types/data.d.ts
@@ -6,7 +6,7 @@ export type Roadmap = {
 
 // プロフィールデータ
 export type Profile = {
-  name?: string;
-  github_account?: string;
-  twitter_account?: string;
+  name: string;
+  github_account: string;
+  twitter_account: string;
 };
