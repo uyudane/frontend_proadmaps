f965bf9313ec482866bff865feadc7dd9018426a
uyudane <sy.yun@icloud.com>
2022-10-29 16:21:17 +0900

    Add: フラッシュメッセージをLayout側で作成するように修正

--- src/component/layout.tsx
+++ src/component/layout.tsx
@@ -1,11 +1,16 @@
-import { Container, Paper } from '@mui/material';
+import { Container, Alert } from '@mui/material';
+import { useRouter } from 'next/router';
 import Footer from './footer';
 import Header from './header';
 
 const Layout = ({ children }: any) => {
+  // フラッシュメッセージを表示
+  const router = useRouter();
+  const message = router.query.message || null;
   return (
     <>
       <Header />
+      {message && <Alert severity='success'>{message}</Alert>}
       {/* Containerはもともと左右にpaddingが設定されているため、上下にのみpaddingをつける */}
       <Container maxWidth='xl' sx={{ pt: 3, pb: 3 }}>
         <main>{children}</main>
