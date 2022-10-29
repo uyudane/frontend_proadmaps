e6026ac63920297a6fd3b768830babb37bff9e74
uyudane <sy.yun@icloud.com>
2022-10-29 21:08:15 +0900

    Add: muiの例を使って、ステップごとの動きを作成

--- src/pages/roadmap/index.tsx
+++ src/pages/roadmap/index.tsx
@@ -1,7 +1,11 @@
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Stepper from '@mui/material/Stepper';
+import Typography from '@mui/material/Typography';
 import { NextPage } from 'next';
-
-import React, { useState } from 'react';
-
+import { useState, ReactNode } from 'react';
 import { useRecoilValue } from 'recoil';
 import Meta from 'component/Meta';
 
@@ -10,61 +14,66 @@ import tokenState from 'recoil/atoms/tokenState';
 
 import { fetchRoadmaps, postRoadmaps } from 'services/roadmaps';
 
+const steps = ['ロードマップ/学習記録の概要', 'ステップ', '確認'];
+
 const RoadmapPage: NextPage = () => {
-  const token = useRecoilValue(tokenState); // RecoilのTokneを取得する
+  const [activeStep, setActiveStep] = useState(0);
 
-  const [title, setTitle] = useState<string>('');
-  const [introduction, setIntroduction] = useState<string>('');
-
-  const onClick_post = () => {
-    postRoadmaps(
-      {
-        title: title,
-        introduction: introduction,
-      },
-      token,
-    );
+  const isStepOptional = (step: number) => {
+    return step === 1;
   };
 
-  // ログでgetの結果を出力
-  const onClick_get = () => {
-    fetchRoadmaps();
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
   };
 
-  const onChange = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    setState: React.Dispatch<React.SetStateAction<string>>,
-  ) => {
-    setState(e.target.value);
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
   };
 
   return (
-    <>
-      <Meta pageTitle='ロードマップ' />
-      <div>
-        <label htmlFor=''>タイトル</label>
-        <input
-          type='text'
-          value={title}
-          onChange={(e) => {
-            onChange(e, setTitle);
-          }}
-        />
-        <br />
-        <label htmlFor=''>本文</label>
-        <input
-          type='text'
-          value={introduction}
-          onChange={(e) => {
-            onChange(e, setIntroduction);
-          }}
-        />
-        <br />
-        <button onClick={onClick_post}>新規投稿</button>
-        <br />
-        <button onClick={onClick_get}>投稿取得</button>
-      </div>
-    </>
+    <Box sx={{ width: '100%' }}>
+      {/* 上部のステップ表示部分 */}
+      {/* Stepperでアクティブの所まで色がつく */}
+      <Stepper activeStep={activeStep}>
+        {steps.map((label, index) => {
+          const stepProps: { completed?: boolean } = {};
+
+          return (
+            <Step key={label} {...stepProps}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+      {activeStep === steps.length ? (
+        <>
+          {/* muiの例だとリセットが出るようになっているが、ここにFinish後の処理を書けば良いと踏んだ。 */}
+          <Typography sx={{ mt: 2, mb: 1 }}>All steps completed - you&apos;re finished</Typography>
+          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+            <Box sx={{ flex: '1 1 auto' }} />
+            <Button onClick={handleReset}>Reset</Button>
+          </Box>
+        </>
+      ) : (
+        <>
+          <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
+          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+            <Button color='inherit' disabled={activeStep === 0} onClick={handleBack} sx={{ mr: 1 }}>
+              Back
+            </Button>
+            <Box sx={{ flex: '1 1 auto' }} />
+            <Button onClick={handleNext}>
+              {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+            </Button>
+          </Box>
+        </>
+      )}
+    </Box>
   );
 };
 
