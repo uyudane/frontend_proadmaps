d2339a5f34a3437d098075ecfb20671a77fcb250
uyudane <sy.yun@icloud.com>
2022-10-31 10:09:22 +0900

    Add: React DnDのsimpleのサンプルコードでドラックアンドドロップを実装

--- src/component/MakeSteps.tsx
+++ src/component/MakeSteps.tsx
@@ -1,8 +1,14 @@
 import { Button, Box } from '@mui/material';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { Container } from './Container';
 
 const MakeSteps = ({ handleNext, handleBack }: { handleNext: any; handleBack: any }) => {
   return (
     <>
+      <DndProvider backend={HTML5Backend}>
+        <Container />
+      </DndProvider>
       ステップの作成
       <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
         <Button color='primary' variant='contained' onClick={handleBack} sx={{ mr: 1 }}>
