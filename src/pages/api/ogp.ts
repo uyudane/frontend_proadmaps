8ea516cc5be643e3d53d0b69cec77f4c228e7c83
uyudane <sy.yun@icloud.com>
2022-11-04 22:15:44 +0900

    Add: URLをひとつに変更。コメントを追加

--- src/pages/api/ogp.ts
+++ src/pages/api/ogp.ts
@@ -3,72 +3,40 @@ import { JSDOM } from 'jsdom';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const ogp = async (req: NextApiRequest, res: NextApiResponse) => {
-  // const targetUrls = extractUrlParams(request, response);
+  const targetUrl = req.query.url as string;
 
-  // res.status(200).json({ text: 'Hello' });
-  console.log(req.query.url);
-  const targetUrls = [];
-  targetUrls.push(req.query.url as string);
-
-  if (!targetUrls) return;
+  if (!targetUrl) return;
 
   const ogps: any = {};
 
-  // リクエストで渡されたURLごとにOGPを取得
-  await Promise.all(
-    targetUrls.map(async (targetUrl: string) => {
-      const encodedUri = encodeURI(targetUrl);
-      const headers = { 'User-Agent': 'bot' };
+  // URLに含まれる幾つかの文字がaxiosの中でエンコードされないため、事前にurlをエンコードしている??
+  const encodedUri = encodeURI(targetUrl);
+  // カスタムヘッダーを設定
+  const headers = { 'User-Agent': 'bot' };
 
-      try {
-        const res = await axios.get(encodedUri, { headers: headers });
-        const html = res.data;
-        const dom = new JSDOM(html);
-        const meta = dom.window.document.head.querySelectorAll('meta');
-        const ogp = extractOgp([...meta]);
+  try {
+    const res = await axios.get(encodedUri, { headers: headers });
+    const html = res.data;
+    const dom = new JSDOM(html);
+    const meta = dom.window.document.head.querySelectorAll('meta');
+    const ogp = extractOgp([...meta]);
 
-        // URLをキーとして、取得したOGPをまとめて返す
-        ogps[targetUrl] = ogp;
-      } catch (error) {
-        console.error(error);
-        // sendErrorResponse(response, error);
-      }
-    }),
-  );
+    // URLをキーとして、取得したOGPをまとめて返す
+    ogps[targetUrl] = ogp;
+  } catch (error) {
+    console.error(error);
+    res.status(400).send('error');
+  }
 
   res.status(200).json(ogps);
 };
 
-// リクエストからOGPを取得しに行くURLを抽出
-// const extractUrlParams = (req: any, res: any): string[] => {
-//   const url = req.query.url;
-//   const urls = req.query.urls;
-
-//   if (url && urls) {
-//     sendErrorResponse(res, "Request query can't have both 'url' and 'urls'");
-//     return [];
-//   } else if (url) {
-//     if (Array.isArray(url)) {
-//       sendErrorResponse(res, "'url' must be string");
-//       return [];
-//     }
-//     return [<string>url];
-//   } else if (urls) {
-//     if (!Array.isArray(urls)) {
-//       sendErrorResponse(res, "'urls' must be array of string");
-//       return [];
-//     }
-//     return <string[]>urls;
-//   } else {
-//     sendErrorResponse(res, "Either 'url' or 'urls' must be included");
-//     return [];
-//   }
-// };
-
 // HTMLのmetaタグからogpを抽出
 const extractOgp = (metaElements: HTMLMetaElement[]): object => {
   const ogp = metaElements
+    // "property"の含む行でフィルタリングし、
     .filter((element: Element) => element.hasAttribute('property'))
+    // meta要素がキーで、バリューがその中身の配列を作成
     .reduce((previous: any, current: Element) => {
       const property = current.getAttribute('property')?.trim();
       if (!property) return;
