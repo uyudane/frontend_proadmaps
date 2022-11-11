ed780bee32f527a93d4fbd592d2148e68ce942b9
uyudane <sy.yun@icloud.com>
2022-11-11 21:19:03 +0900

    Add: ロードマップ作成/編集時の振り分けを作成

--- src/component/RoadmapLikeButton.tsx
+++ src/component/RoadmapLikeButton.tsx
@@ -1,6 +1,7 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import IconButton from '@mui/material/IconButton';
+import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 import RequireLoginDialog from './RequireLoginDialog';
@@ -8,12 +9,13 @@ import tokenState from 'recoil/atoms/tokenState';
 import userState from 'recoil/atoms/userState';
 import { postLike, deleteLike } from 'services/likes';
 
-// ログイン済みの場合は、いいね有無で色、アクションを変える。
-// 未ログイン時はクリック時にログインが必要な旨を通知
 const RoadmapLikeButton = ({ roadmap }: any) => {
   const { isAuthenticated, isLoading } = useAuth0();
   const token = useRecoilValue(tokenState);
   const user = useRecoilValue(userState);
+
+  // パスでロードマップ作成/編集を判断するのに使用
+  const router = useRouter();
 
   // いいねの実施有無を格納
   const [isLiked, setIsLiked] = useState<boolean | null>(null);
@@ -48,12 +50,18 @@ const RoadmapLikeButton = ({ roadmap }: any) => {
 
   return (
     <>
-      {isLoading ? (
+      {router.pathname === '/roadmap/new' || router.pathname === '/drafts/[id]/edit' ? (
+        // ロードマップ作成、編集時の確認画面ではクリックアクションなし。
+        <IconButton aria-label='unfavorite'>
+          <FavoriteIcon sx={{ fontSize: 40 }} />
+        </IconButton>
+      ) : isLoading ? (
         <div>Loading...</div>
       ) : isAuthenticated ? (
         typeof isLiked === 'undefined' ? (
           <p>loading...</p>
         ) : isLiked ? (
+          // ログイン済みの場合は、いいね有無で色、アクションを変える。
           <IconButton aria-label='favorite' onClick={execDeleteLike}>
             <FavoriteIcon sx={{ fontSize: 40, color: 'blue' }} />
           </IconButton>
@@ -63,6 +71,7 @@ const RoadmapLikeButton = ({ roadmap }: any) => {
           </IconButton>
         )
       ) : (
+        // 未ログイン時はクリック時にログインが必要な旨を通知
         <>
           <IconButton aria-label='unfavorite' onClick={dialogOpen}>
             <FavoriteIcon sx={{ fontSize: 40 }} />
