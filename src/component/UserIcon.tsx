bcc34e105fd09467ffd6329515fb2fbcfe1826d1
uyudane <sy.yun@icloud.com>
2022-11-12 19:10:31 +0900

    Add: プロフィール画面、プロフィール編集画面のアイコンをrecoilの情報から出力するように修正

--- src/component/UserIcon.tsx
+++ src/component/UserIcon.tsx
@@ -1,26 +1,15 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { Container, Avatar } from '@mui/material';
 
-const UserIcon = () => {
-  const { isAuthenticated, isLoading, user } = useAuth0();
-
+const UserIcon = ({ user }: any) => {
   return (
     <>
       <Container sx={{ flexGrow: 0 }}>
-        {isAuthenticated ? (
-          <>
-            <Avatar
-              alt='Remy Sharp'
-              src={user!.picture}
-              sx={{ width: { md: 100, lg: 150 }, height: { md: 100, lg: 150 } }}
-            />
-          </>
-        ) : // ログイン確認中はローディングを出す
-        isLoading ? (
-          <div>Loading...</div>
-        ) : (
-          <Avatar alt='Remy Sharp' src='' />
-        )}
+        <Avatar
+          alt='Remy Sharp'
+          src={user.avatar}
+          sx={{ width: { md: 100, lg: 150 }, height: { md: 100, lg: 150 } }}
+        />
       </Container>
     </>
   );
