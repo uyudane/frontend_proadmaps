eae2eed7c53733713050d0cd46b52b6e0e244146
uyudane <sy.yun@icloud.com>
2022-11-13 14:32:43 +0900

    add: フッターの雛形を作成

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,7 +1,40 @@
+import { Grid, Box, Paper, Container, Typography } from '@mui/material';
+import Image from 'next/image';
+
 const Footer = () => {
   return (
     <>
-      <footer>フッター</footer>
+      <Paper
+        sx={{ mt: 'calc(10% + 30px)', width: '100%', bottom: 0, bgcolor: '#eeeeee' }}
+        component='footer'
+        variant='outlined'
+      >
+        <Container maxWidth='lg'>
+          <Box
+            sx={{
+              flexGrow: 1,
+              justifyContent: 'center',
+              display: 'flex',
+              my: 1,
+            }}
+          >
+            <div>
+              <Image priority src='/logo_unit.png' width={16} height={16} alt='Logo' />
+            </div>
+          </Box>
+
+          <Box
+            sx={{
+              flexGrow: 1,
+              justifyContent: 'center',
+              display: 'flex',
+              mb: 2,
+            }}
+          >
+            <Typography variant='caption'>Copyright ©2022. [] Limited</Typography>
+          </Box>
+        </Container>
+      </Paper>
     </>
   );
 };
