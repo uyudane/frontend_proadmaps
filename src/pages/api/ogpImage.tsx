c5adec4cbcd7b5e001ed8b0276ab9a3f83d16773
uyudane <sy.yun@icloud.com>
2022-11-20 14:15:08 +0900

    Add: vercel-ogpの雛形を追加

---
+++ src/pages/api/ogpImage.tsx
@@ -0,0 +1,32 @@
+import { ImageResponse } from '@vercel/og';
+
+export const config = {
+  runtime: 'experimental-edge',
+};
+
+const ogpImage = () => {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 128,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          textAlign: 'center',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        Hello world!
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 600,
+    },
+  );
+};
+
+export default ogpImage;
