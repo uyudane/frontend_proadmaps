f610d0c5979a35457f398437fdaafaae3b04a884
uyudane <sy.yun@icloud.com>
2022-11-21 13:34:46 +0900

    Add: ロードマップAdmin削除機能を追加

--- src/pages/admin/roadmaps/index.tsx
+++ src/pages/admin/roadmaps/index.tsx
@@ -1,24 +1,9 @@
-import { useAuth0 } from '@auth0/auth0-react';
-import { Grid, Box, Container } from '@mui/material';
-import List from '@mui/material/List';
+import { Grid } from '@mui/material';
 import ListItem from '@mui/material/ListItem';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemText from '@mui/material/ListItemText';
-import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
-import type { NextPage } from 'next';
-import { useEffect, useState } from 'react';
-import { useSetRecoilState } from 'recoil'; // Auth0の認証情報をグローバルステートに保存
+import AdminRoadmapDeleteButton from 'component/AdminRoadmapDeleteButton';
 import Meta from 'component/Meta';
-import RoadmapCard from 'component/RoadmapCard';
-import RoadmapEditDeleteButton from 'component/RoadmapEditDeleteButton';
-import SearchModeTabs from 'component/SearchModeTabs';
-import useSearchRoadmaps from 'hooks/useSearchRoadmaps';
 import { useRoadmapsAdmin } from 'services/admin';
-import { getRoadmaps } from 'services/roadmaps';
-import { getTags } from 'services/tags';
-import { getMyUser } from 'services/users';
-import { RoadmapFullData, Tag } from 'types';
 
 const AdminRoadmapsPage = () => {
   const { roadmaps, isLoading, isError } = useRoadmapsAdmin();
@@ -33,13 +18,13 @@ const AdminRoadmapsPage = () => {
             <Grid item xs={10}>
               <ListItemText
                 primary={roadmap.title}
-                secondary={roadmap.introduction}
+                secondary={roadmap.user.name}
                 sx={{ wordWrap: 'break-word' }}
               />
             </Grid>
             <Grid item xs={1}>
               <Grid container alignItems='center' justifyContent='center' direction='column'>
-                <RoadmapEditDeleteButton roadmap={roadmap} />
+                <AdminRoadmapDeleteButton roadmap={roadmap} />
               </Grid>
             </Grid>
           </Grid>
